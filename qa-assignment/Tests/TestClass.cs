b0e9bfd3afe145ff321efc492be47701a1cc4ea2
DESKTOP-9H9IM1S\Richard <richard.zevel@gmail.com>
2022-02-08 17:42:47 +0100

    commited base test structure.

---
+++ qa-assignment/Tests/TestClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Chrome;
+
+public class TestClass
+{
+    public IWebDriver driver;
+
+    [SetUp]
+    public void TestSetup()
+    {
+        driver = new ChromeDriver();
+        driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(30);
+        driver.Navigate().GoToUrl("http://google.com");
+    }
+
+    [Test]
+    public void FirstTest()
+    {
+        Console.WriteLine("Test numero Uno");
+    }
+
+    [TearDown]
+    public void TestTearDown()
+    {
+        driver.Quit();
+    }
+}
